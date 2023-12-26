cdb908a345dced225e4eccdbe023c868761ebe43
Rahul Roy <rahulroy2909@gmail.com>
2023-12-26 00:04:13 -0600

    added improved logic

--- src/rrt.py
+++ src/rrt.py
@@ -1,68 +1,50 @@
-from turtle import distance
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle
+import numpy as np
 import math
 
 class RRT:
-    def __init__(self,vertices,obstacles_count):
-        self.q_initial=[]
-        self.q_goal=[]
-        self.vertices=vertices
-        self.delta=1
-        self.domain=100
-        self.circles=[]
-        self.obstacles=obstacles_count
-        self.list=[]
-        self.prev_list=[[[]]]
+    def __init__(self, vertices, obstacles_count):
+        self.vertices = vertices
+        self.delta = 1
+        self.domain = 100
+        self.circles = []
+        self.obstacles = obstacles_count
+        self.nodes = []
+        self.parents = [[]]
 
     def configuration(self):
-        self.q_random=[np.random.rand()*self.domain,np.random.rand()*self.domain]
-        return self.q_random
-    
-    def nearest_vertex(self):
-        distance=[]
-        for i in self.list:
-            dist=math.dist(i,self.q_random)
-            distance.append(dist)
-        self.q_near=self.list[distance.index(min(distance))]
-        return self.q_near
-    
-    def update_configuration(self):
-        dist=math.dist(self.q_random,self.q_near)
-        x_del = self.q_random[0] - self.q_near[0]
-        y_del = self.q_random[1] - self.q_near[1]
-        q_new_x = self.q_near[0] + (self.delta / dist) * x_del
-        q_new_y = self.q_near[1] + (self.delta / dist) * y_del
-        self.q_new = [q_new_x, q_new_y]
-        return self.q_new
-    
-    def circular_obstacles(self):
-        while self.obstacles!=0:
-            x=np.random.randint(1,100)
-            y=np.random.randint(1,100)
-            radius=np.random.randint(1,8)
-            center=[x,y]
-            self.circles.append([center,radius])
-            self.obstacles-=1
+        return [np.random.rand() * self.domain, np.random.rand() * self.domain]
 
-    def u_check(self, point1, point2, point3):
-        """Calculate u required to check for path collision"""
-        x_delta = point3[0] - point2[0]
-        y_delta = point3[1] - point2[1]
-        u = ((point1[0] - point2[0]) * x_delta + (point1[1] - point2[1]) * y_delta) / (x_delta * x_delta + y_delta * y_delta)
-        return u
-    
-    def vertex_circle_collision(self,vertex,circle):
-        if math.dist(circle[0], vertex) <= circle[1]:
-            return True
+    def nearest_node(self):
+        distances = [math.dist(i, self.q_random) for i in self.nodes]
+        self.q_near = self.nodes[distances.index(min(distances))]
+        return self.q_near
+
+    def new_configuration(self):
+        dist = math.dist(self.q_random, self.q_near)
+        x_delta = self.q_random[0] - self.q_near[0]
+        y_delta = self.q_random[1] - self.q_near[1]
+        self.q_new = [self.q_near[0] + (self.delta / dist) * x_delta, self.q_near[1] + (self.delta / dist) * y_delta]
+        return self.q_new
+
+    def generate_obstacles(self):
+        while self.obstacles != 0:
+            x = np.random.randint(1, 100)
+            y = np.random.randint(1, 100)
+            radius = np.random.randint(1, 8)
+            self.circles.append([[x, y], radius])
+            self.obstacles -= 1
+
+    def check_collision(self, vertex):
+        for circle in self.circles:
+            if math.dist(circle[0], vertex) <= circle[1] or self.path_circle_collision(circle, self.q_near, vertex):
+                return True
         return False
-    
-    def path_collision(self, circle, point1, point2):
-        """Check if the path from a vertex to another vertex intersects with a circle"""
+
+    def path_circle_collision(self, circle, point1, point2):
         x_delta = point2[0] - point1[0]
         y_delta = point2[1] - point1[1]
-        u = self.u_check(circle[0], point1, point2)
+        u = self.calculate_u(circle[0], point1, point2)
         if u < 0:
             closest_point = point1
         elif u > 1:
@@ -74,32 +56,77 @@ class RRT:
         if distance <= circle[1]:
             return True
         return False
-    def check_collision(self, vertex):
-        """Check if there is any collision"""
-        for circle in self.circles:
-            if self.vertex_circle_collision(vertex, circle) or self.path_collision(circle, self.q_near, vertex):
-                return True
-        return False
+
+    def calculate_u(self, point1, point2, point3):
+        x_delta = point3[0] - point2[0]
+        y_delta = point3[1] - point2[1]
+        return ((point1[0] - point2[0]) * x_delta + (point1[1] - point2[1]) * y_delta) / (x_delta * x_delta + y_delta * y_delta)
 
     def check_collision_free_path(self):
-        """Check for a collision free path from a new vertex to the goal"""
         for circle in self.circles:
-            if self.path_collision(circle, self.q_new, self.q_goal):
+            if self.path_circle_collision(circle, self.q_new, self.q_goal):
                 return False
         return True
 
-    def generate_random_start(self):
-        """Generate a random start location"""
+    def visualize(self):
+        f, ax = plt.subplots()
+        plt.ion()
+        ax.set_xlim(0, 100)
+        ax.set_ylim(0, 100)
+        ax.set_title("RRT with Obstacles")
+        self.generate_obstacles()
         self.q_initial = [self.domain * np.random.rand(), self.domain * np.random.rand()]
         for circle in self.circles:
-            while self.vertex_circle_collision(self.q_initial, circle):
+            while math.dist(circle[0], self.q_initial) <= circle[1]:
                 self.q_initial = [self.domain * np.random.rand(), self.domain * np.random.rand()]
-        return self.q_initial
-    
-    def generate_random_goal(self):
-        """Generate a random goal location"""
+        self.nodes.append(self.q_initial)
         self.q_goal = [self.domain * np.random.rand(), self.domain * np.random.rand()]
         for circle in self.circles:
-            while self.vertex_circle_collision(self.q_goal, circle):
+            while math.dist(circle[0], self.q_goal) <= circle[1]:
                 self.q_goal = [self.domain * np.random.rand(), self.domain * np.random.rand()]
-        return self.q_goal
+        ax.plot(self.q_initial[0], self.q_initial[1], 'o', color='green')
+        ax.plot(self.q_goal[0], self.q_goal[1], 'o', color='orange')
+        for circle in self.circles:
+            circle = plt.Circle(circle[0], circle[1], color='black', fill=True)
+            ax.add_artist(circle)
+        for vertex in range(self.vertices):
+            self.q_random = self.configuration()
+            self.q_near = self.nearest_node()
+            self.q_new = self.new_configuration()
+            if not self.check_collision(self.q_new):
+                self.nodes.append(self.q_new)
+                self.parents.append(self.q_near)
+                x1 = [self.q_near[0], self.q_new[0]]
+                y1 = [self.q_near[1], self.q_new[1]]
+                ax.plot(x1, y1, color='blue')
+                plt.pause(0.0001)
+                if self.check_collision_free_path():
+                    x2 = [self.q_new[0], self.q_goal[0]]
+                    y2 = [self.q_new[1], self.q_goal[1]]
+                    ax.plot(x2, y2, color='blue')
+                    plt.pause(0.0001)
+                    break
+        self.nodes.append(self.q_goal)
+        self.parents.append(self.q_new)
+        current = self.nodes[-1]
+        previous = self.parents[-1]
+        while True:
+            if previous == []:
+                break
+            x3 = [current[0], previous[0]]
+            y3 = [current[1], previous[1]]
+            ax.plot(x3, y3, color='red')
+            plt.pause(0.00001)
+            current = previous
+            if current in self.nodes:
+                index = self.nodes.index(current)
+            previous = self.parents[index]
+        plt.show()
+        plt.pause(5)
+
+def main():
+    rrt = RRT(2000, 40)
+    rrt.visualize()
+
+if __name__ == "__main__":
+    main()
