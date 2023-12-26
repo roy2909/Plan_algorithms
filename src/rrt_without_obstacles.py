97f6ddf5861503234895d6b439be6fcf89c938ec
Rahul Roy <rahulroy2909@gmail.com>
2023-12-26 00:17:14 -0600

    added doc-strings

--- src/rrt_without_obstacles.py
+++ src/rrt_without_obstacles.py
@@ -6,7 +6,34 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 class RRT:
+    """
+    Rapidly-Exploring Random Tree (RRT) algorithm for path planning in a 2D space.
+
+    Attributes:
+        domain (tuple): Tuple defining the rectangular space (min, max) for X and Y coordinates.
+        q_init (tuple): Initial configuration (x, y) in the space.
+        delta (float): Step size for extending the tree.
+        K (int): Number of iterations for tree expansion.
+        G (list): List containing the vertices of the RRT.
+        line_segments (list): List of line segments formed by RRT vertices.
+
+    Methods:
+        nearest_neighbor(q_rand): Finds the nearest neighbor of a randomly sampled point.
+        plan(): Constructs the RRT by iteratively expanding the tree.
+        animate_growth(i, scatter, collection): Animation function for visualizing tree growth.
+        animate(): Initializes and displays the RRT animation.
+    """
+
     def __init__(self, domain, q_init, delta, K):
+        """
+        Initializes the RRT with given parameters.
+
+        Args:
+            domain (tuple): Tuple defining the rectangular space (min, max) for X and Y coordinates.
+            q_init (tuple): Initial configuration (x, y) in the space.
+            delta (float): Step size for extending the tree.
+            K (int): Number of iterations for tree expansion.
+        """
         self.domain = domain
         self.q_init = q_init
         self.delta = delta
@@ -15,16 +42,28 @@ class RRT:
         self.line_segments = []
 
     def nearest_neighbor(self, q_rand):
+        """
+        Finds the nearest neighbor of a randomly sampled point in the RRT.
+
+        Args:
+            q_rand (tuple): Randomly sampled point (x, y).
+
+        Returns:
+            tuple: Nearest neighbor to the sampled point.
+        """
         return min(self.G, key=lambda q: ((q[0] - q_rand[0]) ** 2 + (q[1] - q_rand[1]) ** 2) ** 0.5)
 
     def plan(self):
+        """
+        Constructs the RRT by iteratively expanding the tree based on the given parameters.
+        """
         for _ in range(self.K):
             q_rand = (random.uniform(self.domain[0], self.domain[1]),
                       random.uniform(self.domain[0], self.domain[1]))
 
             q_near = self.nearest_neighbor(q_rand)
 
-            dist = (math.sqrt((q_rand[0] - q_near[0]) ** 2 + (q_rand[1] - q_near[1]) ** 2))
+            dist = math.sqrt((q_rand[0] - q_near[0]) ** 2 + (q_rand[1] - q_near[1]) ** 2)
             if dist > self.delta:
                 q_new = (
                     q_near[0] + (self.delta / dist) * (q_rand[0] - q_near[0]),
@@ -37,6 +76,17 @@ class RRT:
             self.G.append(q_new)
 
     def animate_growth(self, i, scatter, collection):
+        """
+        Animation function for visualizing the growth of the RRT.
+
+        Args:
+            i (int): Frame index for the animation.
+            scatter (matplotlib.collections.PathCollection): Scatter plot for RRT vertices.
+            collection (matplotlib.collections.LineCollection): Line collection for RRT edges.
+
+        Returns:
+            tuple: Scatter and collection objects for the animation frame.
+        """
         X_values = [q[0] for q in self.G[:i]]
         Y_values = [q[1] for q in self.G[:i]]
         line_segments = self.line_segments[:i]
@@ -45,6 +95,9 @@ class RRT:
         return scatter, collection
 
     def animate(self):
+        """
+        Initializes and displays the RRT animation.
+        """
         fig, ax = plt.subplots()
         scatter = ax.scatter([], [], s=5, c='r', label='RRT Vertices')
         collection = LineCollection([], colors='black', linewidths=1, alpha=0.5)
